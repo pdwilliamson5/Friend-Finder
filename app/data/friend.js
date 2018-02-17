7693d6bbfbe5cd47cc4e862295a2465d36824186
pdwilliamson5 <30446985+pdwilliamson5@users.noreply.github.com>
2018-02-16 23:56:54 -0600

    Add files via upload

---
+++ app/data/friend.js
@@ -0,0 +1,45 @@
+var friends =
+
+
+{
+  "name":"Ahmed",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      
+    ]
+}
+{
+  "name":"Justin",
+  "photo":"https://goo.gl/images/y4NgJM",
+  "scores":[
+      3,
+      2,
+      6,
+      5,
+      3,
+      2,
+     
+    ]
+}
+{cd 
+  "name":"Genghis",
+  "photo":"https://goo.gl/images/zmaBoJ",
+  "scores":[
+      5,
+      2,
+      4,
+      5,
+      2,
+      1,
+     
+    ]
+}
+
+
+module.export =friends;
